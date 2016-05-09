9d0fe4901d735081db82f1f5fa8d1a5530e7a639
making <makingx@gmail.com>
2016-05-10 02:42:03 +0900

    Support JWT

--- src/main/java/am/ik/home/MoneygrUser.java
+++ src/main/java/am/ik/home/MoneygrUser.java
@@ -1,18 +1,25 @@
 package am.ik.home;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Base64;
 import java.util.Map;
 
 @Component
@@ -25,15 +32,18 @@ public class MoneygrUser implements Serializable {
     private String email;
     private String displayName;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @PostConstruct
-    @SuppressWarnings("unchecked")
-    public void init() {
+    public void init() throws IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         OAuth2Authentication auth = OAuth2Authentication.class.cast(authentication);
-        Map<String, Object> details = (Map<String, Object>) auth.getUserAuthentication().getDetails();
-        this.userId = details.get("id").toString();
-        this.email = details.get("email").toString();
-        Map<String, Object> name = (Map<String, Object>) details.get("name");
-        this.displayName = name.get("familyName") + " " + name.get("givenName");
+        OAuth2AuthenticationDetails details = OAuth2AuthenticationDetails.class.cast(auth.getDetails());
+        String payload = details.getTokenValue().split("\\.")[1];
+        JsonNode json = objectMapper.readValue(Base64Utils.decodeFromUrlSafeString(payload), JsonNode.class);
+        this.userId = json.get("user_id").asText();
+        this.email = json.get("user_name").asText();
+        this.displayName = json.get("display_name").asText();
     }
 }
