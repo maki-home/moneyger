0aede93970048eb92df3e92ae8419045800d7fee
making <makingx@gmail.com>
2016-05-15 17:52:12 +0900

    Add findByOutcomeDate

--- src/main/java/am/ik/home/MoneygrController.java
+++ src/main/java/am/ik/home/MoneygrController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.RequestEntity;
@@ -11,15 +12,18 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @Controller
@@ -38,17 +42,11 @@ public class MoneygrController {
         return "index";
     }
 
-    @RequestMapping("/home")
-    String home(Model model) {
-        Resources<Outcome> outcomes = restTemplate.exchange(
-                RequestEntity.get(UriComponentsBuilder.fromUri(inoutUri).pathSegment("outcomes").build().toUri()).build(),
-                new ParameterizedTypeReference<Resources<Outcome>>() {
-                }
-        ).getBody();
+    Map<String, String> memberMap(Stream<String> ids) {
         JsonNode members = restTemplate.exchange(
                 RequestEntity.get(UriComponentsBuilder.fromUri(memberUri)
                         .pathSegment("members", "search", "findByIds")
-                        .queryParam("ids", outcomes.getContent().stream().map(Outcome::getOutcomeBy).distinct().toArray())
+                        .queryParam("ids", ids.distinct().toArray())
                         .build().toUri()).build(),
                 JsonNode.class)
                 .getBody();
@@ -58,8 +56,38 @@ public class MoneygrController {
                         node -> node.get("memberId").asText(),
                         node -> node.get("familyName").asText() + " " + node.get("givenName").asText()
                 ));
+        return memberMap;
+    }
+
+    @RequestMapping("/home")
+    String home(Model model) {
+        Resources<Outcome> outcomes = restTemplate.exchange(
+                RequestEntity.get(UriComponentsBuilder.fromUri(inoutUri).pathSegment("outcomes").build().toUri()).build(),
+                new ParameterizedTypeReference<Resources<Outcome>>() {
+                }
+        ).getBody();
+        Map<String, String> memberMap = memberMap(outcomes.getContent().stream().map(Outcome::getOutcomeBy));
         outcomes.forEach(o -> o.setMemberMap(memberMap));
         model.addAttribute("outcomes", outcomes);
+        model.addAttribute("total", outcomes.getContent().stream().mapToInt(Outcome::getAmount).sum());
+        model.addAttribute("user", user);
+        return "home";
+    }
+
+    @RequestMapping(path = "outcomes/{outcomeDate}")
+    String showOutcomes(Model model, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable LocalDate outcomeDate) {
+        Resources<Outcome> outcomes = restTemplate.exchange(
+                RequestEntity.get(UriComponentsBuilder.fromUri(inoutUri)
+                        .pathSegment("outcomes", "search", "findByOutcomeDate")
+                        .queryParam("outcomeDate", outcomeDate)
+                        .build().toUri()).build(),
+                new ParameterizedTypeReference<Resources<Outcome>>() {
+                }
+        ).getBody();
+        Map<String, String> memberMap = memberMap(outcomes.getContent().stream().map(Outcome::getOutcomeBy));
+        outcomes.forEach(o -> o.setMemberMap(memberMap));
+        model.addAttribute("outcomes", outcomes);
+        model.addAttribute("total", outcomes.getContent().stream().mapToInt(Outcome::getAmount).sum());
         model.addAttribute("user", user);
         return "home";
     }
