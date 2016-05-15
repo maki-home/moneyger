0802db0cabd281aca3e13a9bbb58a0a7fe5dd30e
making <makingx@gmail.com>
2016-05-15 16:51:17 +0900

    Show member name

--- src/main/java/am/ik/home/MoneygrController.java
+++ src/main/java/am/ik/home/MoneygrController.java
@@ -1,5 +1,6 @@
 package am.ik.home;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class MoneygrController {
@@ -24,6 +30,8 @@ public class MoneygrController {
     MoneygrUser user;
     @Value("${inout.uri:http://localhost:7777/api}")
     URI inoutUri;
+    @Value("${member.uri:http://localhost:9999/uaa/api}")
+    URI memberUri;
 
     @RequestMapping("/")
     String index() {
@@ -37,6 +45,20 @@ public class MoneygrController {
                 new ParameterizedTypeReference<Resources<Outcome>>() {
                 }
         ).getBody();
+        JsonNode members = restTemplate.exchange(
+                RequestEntity.get(UriComponentsBuilder.fromUri(memberUri)
+                        .pathSegment("members", "search", "findByIds")
+                        .queryParam("ids", outcomes.getContent().stream().map(Outcome::getOutcomeBy).distinct().toArray())
+                        .build().toUri()).build(),
+                JsonNode.class)
+                .getBody();
+        Map<String, String> memberMap = StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(members.get("_embedded").get("members").elements(), Spliterator.ORDERED), false)
+                .collect(Collectors.toMap(
+                        node -> node.get("memberId").asText(),
+                        node -> node.get("familyName").asText() + " " + node.get("givenName").asText()
+                ));
+        outcomes.forEach(o -> o.setMemberMap(memberMap));
         model.addAttribute("outcomes", outcomes);
         model.addAttribute("user", user);
         return "home";
--- src/main/java/am/ik/home/Outcome.java
+++ src/main/java/am/ik/home/Outcome.java
@@ -1,10 +1,12 @@
 package am.ik.home;
 
 import lombok.Data;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Map;
 
 @Data
 public class Outcome implements Serializable {
@@ -13,4 +15,14 @@ public class Outcome implements Serializable {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate outcomeDate;
     private String outcomeBy;
+
+    @Setter
+    private Map<String, String> memberMap;
+
+    public String getMemberName() {
+        if (this.memberMap == null || !memberMap.containsKey(this.outcomeBy)) {
+            return "System";
+        }
+        return this.memberMap.get(this.outcomeBy);
+    }
 }
